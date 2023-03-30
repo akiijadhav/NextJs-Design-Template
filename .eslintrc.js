0f21fdf05df9c2705e9759e059d3246c53d36ef9
akhilesh.j@solutelabs.com <akiijadhav22@gmail.com>
2023-03-30 17:42:02 +0530

    edit: eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@next/next/no-html-link-for-pages': ['error', '/src/pages/'], // custom pages path
+    '@next/next/no-html-link-for-pages': ['error'], // custom pages path
   },
 };
